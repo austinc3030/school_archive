a84bf2816dab019ae086a0ed2e9307dfbd9e8f16
austinc3030 <austinc3030@gmail.com>
2020-03-27 15:21:52 -0400

    Add Handlers
    Add the handlers for each page to communicate back to the backend.

---
+++ frontend/js/netlock/loginnew_handler.js
@@ -0,0 +1,51 @@
+// Require jquery
+const jquery = require( 'jquery' )
+window.$ = window.jQuery=jquery;
+
+// Create the client for the server
+const zerorpc = require( "zerorpc" )
+let client = new zerorpc.Client( )
+
+// Get a reference to the formula and result elements
+let formula = document.querySelector( '#formula' )
+let result  = document.querySelector( '#result'  )
+
+// Require some chromium things
+const chromium = require( "chromium" )
+
+
+
+// Connect to the rpc server
+client.connect( "tcp://127.0.0.1:4242" )
+
+
+
+// ****************************************************************************
+// Name: calculate
+// Abstract: Send the formula to the server to calculate it
+// ****************************************************************************
+const calculate = ( ) => {
+  
+  client.invoke( "calc", formula.value, ( error, res ) => {
+  
+    if( error ) {
+  
+      console.error( error )
+  
+    } else {
+  
+      result.textContent = res
+  
+    } // End if
+  
+  }) // End invoke( "calc" )
+
+} // End getScriptPath( )
+
+
+
+// Add an event listener to formula
+formula.addEventListener( 'input', calculate ) // End EventListener
+
+// Calculate the default prefilled equation
+formula.dispatchEvent( new Event( 'input' ) )
