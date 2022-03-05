bdb7f8b9abf1cd534ea54bc03c6fc871ece355be
Brett Marchese <marchebt@mail.uc.edu>
2022-03-04 21:00:20 -0500

    adjustments to have message path and domain

--- zookeeper-ui/zookeeper-ui.cpp
+++ zookeeper-ui/zookeeper-ui.cpp
@@ -16,6 +16,11 @@ int main()
 {
     using namespace boost::locale;
     generator gen;
+
+    //Specify location of dictionaries
+    gen.add_messages_path(".");
+    gen.add_messages_domain("messages");
+
     //One of these linses should be uncommented to allow switching locales
     //std::locale::global(gen("de_DE.UTF - 8");
     std::locale::global(gen(""));
@@ -23,7 +28,7 @@ int main()
 
     ui_main();
 
-    std::cout << translate("\n\n UI Started \n\n");
+    std::cout << translate("UI Started");
 
     system("pause");
 
