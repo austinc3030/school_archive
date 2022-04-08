6fc1a65f88229ceb0e4acfe8a8e0d4c2c0c8263e
Austin Conn <austinc3030@gmail.com>
2022-04-08 18:06:33 -0400

    Implement new functionality to our CAD project

--- AppLibrary/Journaling_FeatureCollection.cpp
+++ AppLibrary/Journaling_FeatureCollection.cpp
@@ -4,6 +4,8 @@
 #include "..\AppPartOps\PartOps.h"
 #include "..\Journaling\Journaling.h"
 #include "..\Journaling\JournalHelpers.h"
+#include "Hole.h"
+#include "HoleBuilder.h"
 
 Application::BlockBuilder* Journaling_FeatureCollection_CreateBlockBuilder(Application::PartFile *part, Application::Block* block)
 {
@@ -27,4 +29,29 @@ Application::BlockBuilder* Journaling_FeatureCollection_CreateBlockBuilder(Appli
 	}
 
 	return retVal;
+}
+
+
+
+Application::HoleBuilder* Journaling_FeatureCollection_CreateHoleBuilder(Application::PartFile* part, Application::Hole* hole)
+{
+
+	if (IsJournaling())
+	{
+
+		JournalStartCall("CreateHoleBuilder", part);
+		JournalInClassParam(hole, "AutomationAPI::Hole", "hole");
+
+	}
+
+	Application::HoleBuilder* holeBuilder = nullptr;
+
+	if (IsJournaling())
+	{
+		JournalReturnClass(holeBuilder, "AutomationAPI::HoleBuilder", "HoleBuilder");
+		JournalEndCall();
+	}
+
+	return holeBuilder;
+
 }
--- AutomationBinding/AutomationAPI_FeatureCollection.h
+++ AutomationBinding/AutomationAPI_FeatureCollection.h
@@ -1,29 +1,33 @@
 #pragma once
 #include "AutomationBindingExports.h"
+
+
+
 namespace AutomationAPI
 {
+
 	class BlockBuilder;
 	class Block;
-	/// <summary>
-	/// FeatureCollection comment
-	/// </summary>
+	class HoleBuilder;
+	class Hole;
+	
+	
+	
 	class AUTOMATIONBINDING_API FeatureCollection
 	{
-		public:
-			FeatureCollection(int guid);
+	
+	public:
+	
+		FeatureCollection(int guid);
 
-			virtual ~FeatureCollection();
+		virtual ~FeatureCollection();
 
-			/// <summary>
-			/// Create a Block builder.
-			/// </summary>
-			/// <param name="block"> if nullptr is passed in this will be created in 
-			/// create mode, otherwise it be in edit mode.</param>
-			/// <returns></returns>
-			BlockBuilder* CreateBlockBuilder(Block* block);
+		BlockBuilder* CreateBlockBuilder(Block* block);
+
+		HoleBuilder* CreateHoleBuilder(Hole* hole);
 
 
-		private:
-			int m_guid;
+	private:
+		int m_guid;
 	};
 }
