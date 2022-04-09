54fb33ad913f1159a4a2e7400e54686bf37831e2
Brett Marchese <marchebt@mail.uc.edu>
2022-04-08 20:02:44 -0400

    Adding API_Refs generated by doxygen

---
+++ API_Refs/html/search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['cadobject_0',['CADObject',['../class_automation_a_p_i_1_1_c_a_d_object.html',1,'AutomationAPI']]],
+  ['commit_1',['Commit',['../class_automation_a_p_i_1_1_block_builder.html#afacdd095f7aaa6ce9586f58681e0cd2a',1,'AutomationAPI::BlockBuilder::Commit()'],['../class_automation_a_p_i_1_1_wire_builder.html#abfc476c658e4bbda9accb63e45b14036',1,'AutomationAPI::WireBuilder::Commit()']]],
+  ['createblockbuilder_2',['CreateBlockBuilder',['../class_automation_a_p_i_1_1_feature_collection.html#acbb82b0fe56005c1a94d0108669a966e',1,'AutomationAPI::FeatureCollection']]]
+];
---
+++ API_Refs/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['commit_0',['Commit',['../class_automation_a_p_i_1_1_block_builder.html#afacdd095f7aaa6ce9586f58681e0cd2a',1,'AutomationAPI::BlockBuilder::Commit()'],['../class_automation_a_p_i_1_1_wire_builder.html#abfc476c658e4bbda9accb63e45b14036',1,'AutomationAPI::WireBuilder::Commit()']]],
+  ['createblockbuilder_1',['CreateBlockBuilder',['../class_automation_a_p_i_1_1_feature_collection.html#acbb82b0fe56005c1a94d0108669a966e',1,'AutomationAPI::FeatureCollection']]]
+];
