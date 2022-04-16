56a43c4a8a369e73305d573a463f73ab7e0b9fd0
Brett Marchese <marchebt@mail.uc.edu>
2022-04-15 22:52:16 -0400

    Documentation and UMLs from Doxygen and Graphviz

---
+++ API_Refs/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['caretupdatepackagetag_0',['CaretUpdatePackageTag',['../struct_caret_update_package_tag.html',1,'']]],
+  ['clearaccessibleselectionfromcontextpackagetag_1',['ClearAccessibleSelectionFromContextPackageTag',['../struct_clear_accessible_selection_from_context_package_tag.html',1,'']]]
+];
---
+++ API_Refs/html/search/classes_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['focusgainedpackagetag_0',['FocusGainedPackageTag',['../struct_focus_gained_package_tag.html',1,'']]],
+  ['focuslostpackagetag_1',['FocusLostPackageTag',['../struct_focus_lost_package_tag.html',1,'']]]
+];
