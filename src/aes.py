61ff670bed97a209d55fda9167e5446dc462c936
Austin Tyler Conn <austinc3030@users.noreply.github.com>
2023-02-14 23:10:14 -0500

    Begin stubbing out code

--- src/aes.py
+++ src/aes.py
@@ -5,42 +5,56 @@ import argparse
 # See progress at "work" to fill in pseudo code
 
 
+class AES(object):
 
-# Make this a class (Overkill for this but nice for using `self.` to reference things)
-def pre_main():
-
-    if sys.version_info<(3,5,0):
-        sys.stderr.write("You need python 3.5 or later to run this script\n")
-        sys.exit(1)
-        
-    try:
+    def __init__(self):
+        pass
+    
+    def _parse_args(self):
         parser = argparse.ArgumentParser()
 
         # Should take in the path to the message file
         parser.add_argument('--message-file', 'message-file', 
                             type=str, 
                             help='The path to the file containing the message '  # Help message is not hard-wrapped
-                                 'to encrypt. Note: The message will be truncated to 128 bits.',
+                                'to encrypt. Note: The message will be truncated to 128 bits.',
                             required=False)
         
         # Should take in the path to the file containing the subkeys
         parser.add_argument('--subkey-file', 'subkey-file',
                             type=str,
                             help='The path to the file containing the subkeys to '  # Help message is not hard-wrapped
-                                 'be used for encryption. Note: Subkeys will be truncated to 128 bits.',
+                                'be used for encryption. Note: Subkeys will be truncated to 128 bits.',
                             required=False)
 
         args=parser.parse_args()
 
-        # Validate arguments given to us
-        # Involves validating the paths and that the file exists
+        return args
+    
+
+    def _validate_args(self, parsed_args):
+        # validate the paths and that the file exists
         # Once files are validated, the message and keys must be validated (and truncated if necessary)
-        # Once all validation/truncation is complete, read message and keys into class variable
+        message_file = parsed_args[0]
+        subkey_file = parsed_args[1]
 
-    except Exception as e:
+        self.message = None
+        self.subkey0 = None
+        self.subkey1 = None
 
-        print(e)
+
+    def main(self):
+
+        return_code = 0
+        self._validate_args(self._parse_args())
+
+        return return_code
 
 
 if __name__ == '__main__':
-    pre_main()
+
+    if sys.version_info<(3,5,0):
+            sys.stderr.write("You need python 3.5 or later to run this script\n")
+            sys.exit(1)
+    else:
+        sys.exit(AES.main())
