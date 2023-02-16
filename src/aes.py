eec2d26d861235185b691841753ca1e4298b7e3e
Austin Tyler Conn <austinc3030@users.noreply.github.com>
2023-02-15 20:19:00 -0500

    Add argument parsing and validate the files given exist

--- src/aes.py
+++ src/aes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
 import argparse
+import os
+import sys
 
 # See progress at "work" to fill in pseudo code
 
@@ -8,47 +9,59 @@ import argparse
 class AES(object):
 
     def __init__(self):
-        pass
-    
+        self.error_count = 0  # Indicates success
+        self.exit_message = ""  # Empty exit message
+
+        self.message_file = None
+        self.subkey_file = None
+
+
     def _parse_args(self):
         parser = argparse.ArgumentParser()
 
         # Should take in the path to the message file
-        parser.add_argument('--message-file', 'message-file', 
+        parser.add_argument('--message-file', 
                             type=str, 
                             help='The path to the file containing the message '  # Help message is not hard-wrapped
                                 'to encrypt. Note: The message will be truncated to 128 bits.',
                             required=False)
         
         # Should take in the path to the file containing the subkeys
-        parser.add_argument('--subkey-file', 'subkey-file',
+        parser.add_argument('--subkey-file',
                             type=str,
                             help='The path to the file containing the subkeys to '  # Help message is not hard-wrapped
                                 'be used for encryption. Note: Subkeys will be truncated to 128 bits.',
                             required=False)
 
-        args=parser.parse_args()
+        parsed_arguments=parser.parse_args()
 
-        return args
+        return parsed_arguments
     
 
-    def _validate_args(self, parsed_args):
-        # validate the paths and that the file exists
-        # Once files are validated, the message and keys must be validated (and truncated if necessary)
-        message_file = parsed_args[0]
-        subkey_file = parsed_args[1]
+    def _validate_args(self, parsed_arguments):
+        # Validate message file exists
+        if not os.path.isfile(os.path.abspath(parsed_arguments.message_file)):
+            self.error_count += 1
+            self.exit_message += 'Error {error_count}: --message-file "{message_file}" does not exist.\n' \
+                                 .format(error_count=self.error_count, message_file=parsed_arguments.message_file)
+        else:
+            self.message_file = os.path.abspath(parsed_arguments.message_file)
+        
+        # Validate subkey file exists
+        if not os.path.isfile(os.path.abspath(parsed_arguments.subkey_file)):
+            self.error_count += 1
+            self.exit_message += 'Error {error_count}: --subkey-file "{subkey_file}" does not exist.\n' \
+                                 .format(error_count=self.error_count, subkey_file=parsed_arguments.subkey_file)
+        else:
+            self.subkey_file = os.path.abspath(parsed_arguments.subkey_file)
 
-        self.message = None
-        self.subkey0 = None
-        self.subkey1 = None
+        return (self.message_file and self.subkey_file)
 
 
     def main(self):
 
-        return_code = 0
-        self._validate_args(self._parse_args())
-
-        return return_code
+        # Make sure we got valid args
+        if self._validate_args(self._parse_args()):
 
 
 if __name__ == '__main__':
@@ -57,4 +70,8 @@ if __name__ == '__main__':
             sys.stderr.write("You need python 3.5 or later to run this script\n")
             sys.exit(1)
     else:
-        sys.exit(AES.main())
+        aes = AES()
+        aes.main()
+        if aes.error_count > 0:
+             print(aes.exit_message)
+        sys.exit(not(aes.error_count == 0))
