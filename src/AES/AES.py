c1d5fde7c2ded59ec16c361c774e8391f8fa1398
Conn <M64200@NGC.AUTHORITY>
2023-02-16 15:04:04 -0500

    Formatting. Remove unneccesary print statement

--- src/AES/AES.py
+++ src/AES/AES.py
@@ -9,26 +9,12 @@ from AES.Utilities import initialize_state
 class AES(object):
 
     def _obtain_initial_state(self):
-        # Obtain initial state
-        # Map each character to ascii
-        # T  w  o     O  n  e     N  i  n  e     T  w  o
-        # 54 77 6F 20 4F 6E 65 20 4E 69 6E 65 20 54 77 6F
-        # Initialize State (4 by 4 array of bytes) with message
-        # Divide into 4 parts
-        # 54776F20    4F6E6520    4E696E65       2054776F
-        # State should be
-        # 54  4F  4E  20
-        # 77  6E  69  54
-        # 6F  65  6E  77
-        # 20  20  65  6F
-
-        state = initialize_state(self.message) 
-        print(state)
+        self.initial_state = initialize_state(self.message)
 
 
     def main(self):
         self._obtain_initial_state()
-
+        
 
     def __init__(self):
         self.error_handler = ErrorHandler()
