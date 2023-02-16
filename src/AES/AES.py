a5400155545ffaf103e312c324b6130d95b59f02
Conn <M64200@NGC.AUTHORITY>
2023-02-16 15:02:33 -0500

    Initialize State

--- src/AES/AES.py
+++ src/AES/AES.py
@@ -2,17 +2,39 @@ import sys
 
 from AES.ArgumentHandler import ArgumentHandler
 from AES.ErrorHandler import ErrorHandler
+from AES.Utilities import initialize_state
 
 
 
-class AES(object):        
+class AES(object):
+
+    def _obtain_initial_state(self):
+        # Obtain initial state
+        # Map each character to ascii
+        # T  w  o     O  n  e     N  i  n  e     T  w  o
+        # 54 77 6F 20 4F 6E 65 20 4E 69 6E 65 20 54 77 6F
+        # Initialize State (4 by 4 array of bytes) with message
+        # Divide into 4 parts
+        # 54776F20    4F6E6520    4E696E65       2054776F
+        # State should be
+        # 54  4F  4E  20
+        # 77  6E  69  54
+        # 6F  65  6E  77
+        # 20  20  65  6F
+
+        state = initialize_state(self.message) 
+        print(state)
+
 
     def main(self):
-        pass
+        self._obtain_initial_state()
 
 
     def __init__(self):
         self.error_handler = ErrorHandler()
         self.argument_handler = ArgumentHandler(error_handler=self.error_handler)
         if self.argument_handler.handle_arguments():
+            self.message = self.argument_handler.message_to_encrypt
+            self.subkey0 = self.argument_handler.subkey0
+            self.subkey1 = self.argument_handler.subkey1
             self.main()
--- src/AES/Utilities.py
+++ src/AES/Utilities.py
@@ -23,3 +23,71 @@ def convert_hex_to_binary(string_to_convert):
             hex_number = ""
 
     return binary_result
+
+
+def string_to_hex(string_to_convert):
+    hex_string = ""
+
+    for character in string_to_convert:
+        hex_string += "{0:02x}".format(ord(character))
+
+    return hex_string
+
+
+def split_hex_string(string_to_split):
+    # Map each character to ascii
+    # T  w  o     O  n  e     N  i  n  e     T  w  o
+    # 54 77 6F 20 4F 6E 65 20 4E 69 6E 65 20 54 77 6F
+    split_hex = []
+    hex_number = ""
+
+    for index, character in enumerate(string_to_split):
+        if len(hex_number) == 0:
+            hex_number += character
+        elif len(hex_number) == 1:
+            hex_number += character
+
+        if len(hex_number) == 2:
+            split_hex.append(hex_number)
+            hex_number = ""
+
+    return split_hex
+
+
+def chunk_message(message_to_chunk, number_of_chunks=4):
+    # Initialize State (4 by 4 array of bytes) with message
+    # Divide into 4 parts
+    # 54776F20    4F6E6520    4E696E65       2054776F
+    chunked_message = []
+
+    split_message = split_hex_string(string_to_hex(message_to_chunk))
+    chunk_length = int(len(message_to_chunk) / number_of_chunks)
+    
+    for message_index in range(0, number_of_chunks):
+        message_chunk = []
+        for chunk_index in range(0, chunk_length):
+            message_chunk.append(split_message[message_index * chunk_length + chunk_index])
+        chunked_message.append(message_chunk)
+    
+    return chunked_message
+
+
+def initialize_state(message_for_state):
+    # From the chunked message, it should go
+    # 54776F20    4F6E6520    4E696E65       2054776F
+    # Indexes              Example output
+    # 00, 10, 20, 30       54  4F  4E  20
+    # 01, 11, 21, 31 ____\ 77  6E  69  54
+    # 02, 12, 22, 32     / 6F  65  6E  77
+    # 03, 13, 23, 33       20  20  65  6F
+    state = []
+
+    chunked_message = chunk_message(message_to_chunk=message_for_state, number_of_chunks=4)
+        
+    for row_index in range(0, 4):
+        row = []
+        for column_index in range(0, 4):
+            row.append(chunked_message[column_index][row_index])
+        state.append(row)
+
+    return state
